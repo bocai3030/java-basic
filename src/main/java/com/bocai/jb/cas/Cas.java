1fe9c848010441ec803867bff1f479f7a079c523
dinglinbo <dinglinbo@gomo.com>
2015-10-08 22:37:47 +0800

    test synchronized atomic ReentrantLock

--- src/main/java/com/bocai/jb/cas/Cas.java
+++ src/main/java/com/bocai/jb/cas/Cas.java
@@ -1,9 +1,15 @@
 package com.bocai.jb.cas;
 
-import java.util.Random;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 public class Cas {
     public void simpleIncrement() {
@@ -12,39 +18,85 @@ public class Cas {
         System.out.println(atomicInteger);
     }
 
-    public void compareAtomicAndSynchronized(final int calcCount) {
+    static int value = 0;
+
+    public void compareSynchronizedAndAtomicAndReentrantLock(final int calcCount) {
         Preconditions.checkArgument(calcCount > 0);
 
         long startTime = 0, endTime = 0;
-        final Random random = new Random(System.currentTimeMillis());
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+        final List<Future<Void>> futures = Lists.newArrayList();
+
+        Cas.value = 0;
+        final Object lock1 = new Object();
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < calcCount; i++) {
+            futures.add(executorService.submit(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    synchronized (lock1) {
+                        ++Cas.value;
+                    }
+                    return null;
+                }
+            }));
+        }
+        for (final Future<Void> future : futures) {
+            try {
+                future.get();
+            } catch (final Exception e) {
+            }
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(String.format("normal ++int %d times cost %d ms from %d to %d, value is %d", calcCount, endTime - startTime, startTime, endTime, Cas.value));
 
         final AtomicInteger atomicInteger1 = new AtomicInteger(0);
-        final AtomicInteger atomicInteger2 = new AtomicInteger(0);
         startTime = System.currentTimeMillis();
         for (int i = 0; i < calcCount; i++) {
-            atomicInteger1.addAndGet(random.nextInt());
-            atomicInteger2.addAndGet(random.nextInt());
+            futures.add(executorService.submit(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    atomicInteger1.incrementAndGet();
+                    return null;
+                }
+            }));
         }
-        endTime = System.currentTimeMillis();
-        System.out
-                .println(String.format("AtomicInteger cas %d times cost %d ms from %d to %d, atomicInteger is %d,%d", calcCount, endTime - startTime, startTime, endTime, atomicInteger1.intValue(), atomicInteger2
-                        .intValue()));
-
-        int normalInteger1 = 0;
-        int normalInteger2 = 0;
-        final Object lock1 = new Object();
-        final Object lock2 = new Object();
-        startTime = System.currentTimeMillis();
-        for (int i = 0; i < calcCount; i++) {
-            synchronized (lock1) {
-                normalInteger1 += random.nextInt();
-            }
-            synchronized (lock2) {
-                normalInteger2 += random.nextInt();
+        for (final Future<Void> future : futures) {
+            try {
+                future.get();
+            } catch (final Exception e) {
             }
         }
         endTime = System.currentTimeMillis();
-        System.out.println(String.format("normal ++int %d times cost %d ms from %d to %d, normalInteger is %d,%d", calcCount, endTime - startTime, startTime, endTime, normalInteger1, normalInteger2));
+        System.out.println(String.format("AtomicInteger cas %d times cost %d ms from %d to %d, atomicInteger is %d", calcCount, endTime - startTime, startTime, endTime, atomicInteger1.intValue()));
 
+        Cas.value = 0;
+        final ReentrantLock lock = new ReentrantLock();
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < calcCount; i++) {
+            futures.add(executorService.submit(new Callable<Void>() {
+                @Override
+                public Void call() throws Exception {
+                    lock.lock();
+                    try {
+                        ++Cas.value;
+                    } finally {
+                        lock.unlock();
+                    }
+                    return null;
+                }
+            }));
+        }
+        for (final Future<Void> future : futures) {
+            try {
+                future.get();
+            } catch (final Exception e) {
+            }
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(String.format("ReentrantLock %d times cost %d ms from %d to %d, value is %d", calcCount, endTime - startTime, startTime, endTime, Cas.value));
+
+        executorService.shutdown();
     }
 }
--- src/test/java/com/bocai/jb/cas/TestCas.java
+++ src/test/java/com/bocai/jb/cas/TestCas.java
@@ -17,31 +17,18 @@ public class TestCas {
     }
 
     @Test
-    public void testCompareAtomicAndSynchronized() {
-        int calcCount = 1000000;
-        this.cas.compareAtomicAndSynchronized(calcCount);
+    public void testCompareSynchronizedAndAtomicAndReentrantLock() {
+        int calcCount = 10000;
+        this.cas.compareSynchronizedAndAtomicAndReentrantLock(calcCount);
         System.out.println();
 
         calcCount *= 10;
-        this.cas.compareAtomicAndSynchronized(calcCount);
+        this.cas.compareSynchronizedAndAtomicAndReentrantLock(calcCount);
         System.out.println();
 
         calcCount *= 10;
-        this.cas.compareAtomicAndSynchronized(calcCount);
+        this.cas.compareSynchronizedAndAtomicAndReentrantLock(calcCount);
         System.out.println();
-
-        // need to explain the following result in my local machine:
-
-        // AtomicInteger cas 1000000 times cost 43 ms from 1444312152502 to 1444312152545, atomicInteger is 66355847,-737429448
-        // normal ++int 1000000 times cost 59 ms from 1444312152547 to 1444312152606, normalInteger is -249900303,1729242971
-        //
-        // AtomicInteger cas 10000000 times cost 348 ms from 1444312152606 to 1444312152954, atomicInteger is -1054062048,-433214107
-        // normal ++int 10000000 times cost 294 ms from 1444312152954 to 1444312153248, normalInteger is 1828511479,-1762278322
-        //
-        // AtomicInteger cas 100000000 times cost 3191 ms from 1444312153248 to 1444312156439, atomicInteger is -991743484,-1318598102
-        // normal ++int 100000000 times cost 3194 ms from 1444312156440 to 1444312159634, normalInteger is 1975706487,1942196291
-
-        // There maybe exists optimize in the os level. JUST GUESS.
     }
 
 }
